46e9b9bfc2f91aaeee82504d53e19c98c0a8dbbe
rezakarim1585 <74957734+rezakarim1585@users.noreply.github.com>
2022-12-22 02:43:41 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -152,7 +152,7 @@ st.pyplot(fig)
 
 
 
-st.bar_chart(data=df_pred, x=None, y=None, width=0, height=0, use_container_width=True)
+st.line_chart(data=df_pred, x='ML Algorithm', y='Predicted Shear Strength (KN)', width=0, height=0, use_container_width=True)
 #st.write(p_lr,p_rr,p_lasr,p_svr,p_dt,p_rf,p_xb,p_ab,p_knn,p_ann)
 st.write('---')
 
